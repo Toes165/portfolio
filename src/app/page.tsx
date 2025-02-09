f44fe80228898a1b8f6594da6df06385916caaab
Anderson To <100450830+Toes165@users.noreply.github.com>
2025-02-08 16:51:29 -0800

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,118 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import "./page.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <>
+      <nav className="navbar">
+        <Link href={"#Home"} className="links">
+          Home
+        </Link>
+        <Link href={"#Projects"} className="links">
+          Projects
+        </Link>
+        <Link href={"#About"} className="links">
+          About
+        </Link>
+        <Link href={"#Contact"} className="links">
+          Contact
+        </Link>
+      </nav>
+      <section className="HomePage parallax" id="Home">
+        <div className="Intro">
+          <h1>Hello!</h1>
+          <h1>Welcome</h1>
+          <h1>to</h1>
+          <h1>my</h1>
+          <h1>Portfolio!</h1>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+      <section className="ProjectsPage" id="Projects">
+        <div className="card">
+          <img src="ayo.jpg" alt="Project Image" />
+          <h1>Project 1</h1>
+          <div className="project-btn">
+            <button>GitHub</button>
+            <button>Demo</button>
+          </div>
+        </div>
+        <div className="card">
+          <img src="ayo.jpg" alt="Project Image" />
+          <h1>Project 2</h1>
+          <div className="project-btn">
+            <button>GitHub</button>
+            <button>Demo</button>
+          </div>
+        </div>
+        <div className="card">
+          <img src="ayo.jpg" alt="Project Image" />
+          <h1>Project 3</h1>
+          <div className="project-btn">
+            <button>GitHub</button>
+            <button>Demo</button>
+          </div>
+        </div>
+        <div className="card">
+          <img src="ayo.jpg" alt="Project Image" />
+          <h1>Project 4</h1>
+          <div className="project-btn">
+            <button>GitHub</button>
+            <button>Demo</button>
+          </div>
+        </div>
+      </section>
+      <section className="AboutPage" id="About">
+        <div className="about-img">
+          <img src="/goofy.png" alt="Anderson" />
+        </div>
+        <div className="AboutMe">
+          <h3>Hello,</h3>
+          <p>My name is Anderson!</p>
+          <p>
+            I am an undergraduate Computer Science student at Bellevue College.
+            As a first-generation student, I aspire to break into the tech
+            industry, leveraging my diverse background to develop innovative and
+            impactful solutions.
+          </p>
+          <p>
+            My interests span full-stack development, artificial intelligence,
+            and software engineering. My passion for software engineering drives
+            me to design accesible, maintainable, and efficient code while
+            continuously learning about new technologies.
+          </p>
+          <p>
+            With experience in front-end development and data analysis, I am
+            proficient in Java, Python, C/C++, JavaScript, TypeScript, and
+            frameworks like React and Next.js.
+          </p>
+          <p>
+            Beyond academics, I enjoy listening to music, gaming, photography,
+            exercising, and exploring new restaurants.
+          </p>
+          <p>
+            I'm always open to questions and conversations—feel free to reach
+            out!
+          </p>
+        </div>
+        <div className="about-buttons">
+          <button>Email</button>
+          <button>LinkedIn</button>
+          <button>GitHub</button>
+        </div>
+      </section>
+      <section className="ContactPage" id="Contact">
+        <div className="Contact">
+          <form action="">
+            <input type="text" placeholder="Name" />
+            <br />
+            <input type="email" placeholder="Email" />
+            <br />
+            <input type="text" placeholder="Message" />
+            <br />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      </section>
+    </>
   );
 }
